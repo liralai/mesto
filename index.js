d312a0c383640c1d699ef3983df6e9b283d3d14c
Anastasiia Kudinova <nensantre@yandex.ru>
2023-05-22 00:23:36 +0300

    feat: добавить скрипт для попапа

---
+++ index.js
@@ -0,0 +1,30 @@
+const profileEditButtonEl = document.querySelector('.profile__edit-button');
+const popupSaveButtonEl = document.querySelector('.popup__save-button');
+const popupCloseButtonEl = document.querySelector('.popup__close-button');
+const popupEl = document.querySelector('.popup');
+const nameInputEl = document.querySelector('#name-input');
+const infoInputEl = document.querySelector('#info-input');
+const profileNameEl = document.querySelector('.profile__name');
+const profileTitleEl = document.querySelector('.profile__title');
+
+nameInputEl.value = profileNameEl.textContent;
+infoInputEl.value = profileTitleEl.textContent;
+
+profileEditButtonEl.addEventListener('click', function(){
+  popupEl.classList.add('popup_is-opened');
+});
+
+popupCloseButtonEl.addEventListener('click', function(){
+  popupEl.classList.remove('popup_is-opened');
+});
+
+popupSaveButtonEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  profileNameEl.textContent = nameInputEl.value;
+  profileTitleEl.textContent = infoInputEl.value;
+
+  popupCloseButtonEl.addEventListener('click', function(){
+    popupEl.classList.remove('popup_is-opened');
+  });
+});
